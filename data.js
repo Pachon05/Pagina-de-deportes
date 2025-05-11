4eb36164a2425178ae2e7876e225d14294e71ecc
Stevennn31 <162370829+Stevennn31@users.noreply.github.com>
2025-05-11 16:29:53 -0500

    data.js

--- data.js
+++ data.js
@@ -35,6 +35,24 @@ const data = {
       autor: "Luis Méndez",
       lugar: "Londres, Inglaterra",
       imagen: "https://ichef.bbci.co.uk/ace/standard/3840/cpsprodpb/b064/live/8f9e7040-20a6-11ef-80aa-699d54c46324.jpg"
+    },
+    {
+      titulo: "El Barcelona sentencia LaLiga contra el Real Madrid.",
+      resumen: "En un gran choque en Montjuic, el Barcelona remontó dos goles de Mbappé, ganó y es virtualmente campeón. (4-3)",
+      contenido:`
+        <p>En un partido tremendo, el Barcelona decidió LaLiga a falta de tres partidos para que termine el campeonato. Por cuarta vez esta temporada, ganó al Real Madrid y confirmó que es el mejor equipo.</p>
+        <p>Flick ha construido un conjunto ganador, que cuando se pone voraz es implacable, pero que duda atrás y muestra debilidad demasiadas veces.</p>
+        <p>Cuando se puso, en la primera parte, fue una tormenta contra el equipo de Carlo Ancelotti, sin ningún recurso para evitar la presión o para jugar a algo. El líder metió cuatro goles y dejó en anécdota los dos que había hecho antes Kylian Mbappé.</p>
+        <p>Detalles completos en 
+          <a href="https://www.larazon.es/deportes/futbol/barcelona-real-madrid-directo-hoy-previa-alineaciones-ultima-hora-clasico_20250511681c8bc3319ae75da4c551f2.html" target="_blank">
+            LaRazón
+          </a>.
+        </p>
+      `,
+      fecha: "11 de mayo de 2025",
+      autor: "Sebastian Parra",
+      lugar: "Madrid, España",
+      imagen: "https://theobjective.com/wp-content/uploads/2025/05/barcelona-real-madrid-resumen-goles-resultado.jpg"
     }
   ],
   basquetbol: [
@@ -73,6 +91,24 @@ const data = {
       autor: "Ana López",
       lugar: "Chicago, EE.UU.",
       imagen: "https://enkq8rj78bi.exactdn.com/wp-content/uploads/2025/02/NBA-All-Star-2025-1.jpg"
+    },
+    {
+      titulo: "¡Los Celtics se impusieron en el Madison Square Garden!",
+      resumen: "A base de triples (20 en total, 50 % de efectividad), Boston superó a New York por 115 a 93 y puso las semifinales del Este 1-2. Pritchard aportó 23 unidades y Tatum sumó 22 puntos. Brunson fue el mejor en los rivales con 27.",
+      contenido: `
+        <p>Con orgullo de campeón y una excelente puntería desde el perímetro, Boston Celtics asaltó este sábado el Madison Square Garden, arrolló a New York Knicks por 115-93 y colocó 2-1 esta semifinal del Este.</p>
+        <p>El cuarto partido de esta vibrante eliminatoria se jugará el lunes también en la Gran Manzana.</p>
+        <p>Los defensores del título llegaban a Nueva York tras dos dolorosas y muy preocupantes derrotas en Boston, ambas desperdiciando un +20 en el tercer cuarto y con sensaciones terribles en el desenlace.</p>
+        <p>Detalles completos en 
+          <a href="https://www.espn.com.co/basquetbol/nota/_/id/15166766/nba-playoffs-resultados-sabado-celtics-se-recupero-ante-knicks-y-timberwolves-saco-ventaja-contra-warriors" target="_blank">
+            ESPN Deportes
+          </a>.
+        </p>
+      `,
+      fecha: "11 de mayo de 2025",
+      autor: "ESPN Digital",
+      lugar: "Londres, Inglaterra",
+      imagen: "https://a.espncdn.com/combiner/i?img=%2Fphoto%2F2025%2F0511%2Fr1491038_1296x729_16%2D9.jpg&w=920&h=518&scale=crop&cquality=80&location=origin&format=jpg"
     }
   ],
   tenis: [
@@ -111,6 +147,24 @@ const data = {
       autor: "Fernando Ríos",
       lugar: "París, Francia",
       imagen: "https://cdn-3.expansion.mx/dims4/default/1b8a502/2147483647/strip/true/crop/4395x2930+0+0/resize/1200x800!/format/webp/quality/60/?url=https%3A%2F%2Fcdn-3.expansion.mx%2F29%2F7a%2F042e9ea74640a0e19654bd7e9038%2F000-36jr4n7.jpg"
+    },
+    {
+      titulo: "Roma ya tiene a su primera víctima ilustre.",
+      resumen: "El tenista búlgaro Grigor Dimitrov estrella la raqueta contra el suelo hasta destrozarla. Perdió ante el italiano Francesco Passaro en la segunda ronda del Masters 1000 ATP de Roma",
+      contenido: `
+        <p>Héroe y villano en el Masters 1000 ATP de Roma. El ídolo fue de casa, dentro de la pléyade de tenistas italianos que está inundando de talento el circuito profesional masculino.</p>
+        <p>El papel de malo de la película fue adjudicado a un tenista que se caracteriza más bien por su bondad dentro y fuera de la pista. Es uno de los considerados caballeros del ATP Tour, tras una larga e impecable trayectoria.</p>
+        <p>Todos tienen su día horrible, cuando el temperamento se desborda. E, impotente ante un rival teóricamente tan inferior, pero impulsado por la grada, el otrora frío y controlado búlgaro Grigor Dimitrov explotó.</p>
+        <p>Lee más en 
+          <a href="https://www.mundodeportivo.com/tenis/20250509/1002460866/roma-primera-victima-ilustre.html" target="_blank">
+            Mundo Deportivo
+          </a>.
+        </p>
+      `,
+      fecha: "9 de mayo de 2025",
+      autor: "Angel Rigueira",
+      lugar: "Roma, Italia",
+      imagen: "https://www.mundodeportivo.com/files/image_948_465/files/fp/uploads/2025/05/09/681dfe1e691ee.r_d.2788-2288-1691.jpeg"
     }
   ]
 };
